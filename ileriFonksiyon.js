0d282f386727a32c469b952a1c80adf4b4785d25
Mert <kmert0@outlook.com>
2022-08-01 16:46:41 +0300

    first commit

---
+++ ileriFonksiyon.js
@@ -0,0 +1,24 @@
+// var selamVerFonksiyonu = function selamVer(){
+//     console.log("Merhaba")
+// }
+
+// selamVerFonksiyonu();
+
+// const selamVerFonksiyonu2 = () =>{
+//     console.log("Merhaba 2");
+// }
+
+// selamVerFonksiyonu2();
+
+// const selamVerFonksiyonu3 = (mesaj) =>{
+//     console.log(mesaj);
+// }
+
+// selamVerFonksiyonu3("Hello World");
+
+var topla = (sayi1, sayi2) => {
+  return sayi1 + sayi2;
+};
+
+let toplam = topla(3, 4);
+console.log(toplam);
