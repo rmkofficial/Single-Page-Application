23149f9eefc663204a207106c38429a85b56776c
Mert <kmert0@outlook.com>
2022-08-01 15:27:40 +0300

    first commit

--- dom.js
+++ dom.js
@@ -49,4 +49,8 @@ var p2 = document.getElementById("p2");
 div1.insertBefore(baslik,p2)
 
 alert("p2 siliniyor");
-div1.removeChild(p2)
+div1.removeChild(p2);
+
+alert("Degistiriliyor");
+var p1 = document.getElementById("p1");
+div1.replaceChild(baslik,p1);
