e9a31bd75727694058243ad69765d72a0407c692
Mert <kmert0@outlook.com>
2022-08-11 11:13:29 +0300

    first event

--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -9,18 +9,22 @@ export default class CategoryList extends Component {
     ],
     currentCategory: "",
   };
-    changeCategory = (category) => {
-      this.setState({ currentCategory: category.categoryName });
-    };
-  }
+
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+  };
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
         <ListGroup>
           {this.state.categories.map((category) => (
-            <ListGroupItem key={category.categoryId}>{category.categoryId}</ListGroupItem>,
-            <ListGroupItem key={category.categoryName}>{category.categoryName}</ListGroupItem>
+            <ListGroupItem
+              onClick={() => this.changeCategory(category)}
+              key={category.categoryId}
+            >
+              {category.categoryName}
+            </ListGroupItem>
           ))}
         </ListGroup>
         <h4>{this.state.currentCategory}</h4>
