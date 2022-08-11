0b4b230d138243717bbf780b93ff9a764f8c34ca
Mert <kmert0@outlook.com>
2022-08-11 11:36:14 +0300

    componentler arası event transferi

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,29 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import CategoryList from "./CategoryList";
 import Navi from "./Navi";
 import ProductList from "./ProductList";
 import { Col, Container, Row } from "reactstrap";
 
-function App() {
-  let productInfo = { title: "ProductList" };
-  let categoryInfo = { title: "CateforyList" };
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Navi></Navi>
-        </Row>
-        <Row>
-          <Col xs="3">
-            <CategoryList info={categoryInfo}></CategoryList>
-          </Col>
-          <Col xs="9">
-            <ProductList info={productInfo}></ProductList>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+export default class App extends Component {
+  state={currentCategory:""}
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+  };
+  render() {
+    let productInfo = { title: "ProductList" };
+    let categoryInfo = { title: "CateforyList" };
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi></Navi>
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo}></CategoryList>
+            </Col>
+            <Col xs="9">
+              <ProductList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={productInfo}></ProductList>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default App;
