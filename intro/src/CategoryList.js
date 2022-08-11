e14e559d240740a5d78ef13d1b96e896d57908ee
Mert <kmert0@outlook.com>
2022-08-11 09:54:59 +0300

    Bug fixed.

--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -9,6 +9,10 @@ export default class CategoryList extends Component {
         { categoryId: 1, categoryName: "Beverages" },
         { categoryId: 2, categoryName: "Condiments" },
       ],
+      currentCategory: "",
+    };
+    changeCategory = (category) => {
+      this.setState({ currentCategory: category.categoryName });
     };
   }
   render() {
@@ -21,6 +25,7 @@ export default class CategoryList extends Component {
             <ListGroupItem key={category.categoryName}>{category.categoryName}</ListGroupItem>
           ))}
         </ListGroup>
+        <h4>{this.state.currentCategory}</h4>
       </div>
     );
   }
